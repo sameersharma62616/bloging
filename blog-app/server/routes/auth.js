e14806647f8ff2b9ac49bca6c1be0cdf556f4aa7
sameersharma62616 <raj6264640954@gmail.com>
2025-07-05 16:56:18 +0530

    First commit

---
+++ blog-app/server/routes/auth.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Register
+router.post('/register', async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const user = new User({ ...req.body, password: hashedPassword });
+  await user.save();
+  res.json("User registered");
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json("User not found");
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.status(400).json("Wrong password");
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token, userId: user._id });
+});
+
+module.exports = router;
---
+++ blog-app/server/routes/blogs.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const Blog = require('../models/Blog');
+const jwt = require('jsonwebtoken');
+
+// Middleware to verify token
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(403).send("Token missing");
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch {
+    res.status(401).send("Invalid token");
+  }
+};
+
+// CREATE
+router.post('/', verifyToken, async (req, res) => {
+  const blog = new Blog({ ...req.body, author: req.userId });
+  await blog.save();
+  res.json(blog);
+});
+
+// for image or video
+router.post("/", verifyToken, async (req, res) => {
+  const { title, content, image, video } = req.body;
+  const blog = new Blog({
+    title,
+    content,
+    image,
+    video,
+    author: req.userId,
+  });
+  await blog.save();
+  res.json(blog);
+});
+
+// READ ALL
+router.get('/', async (req, res) => {
+  const blogs = await Blog.find().populate('author', 'username');
+  res.json(blogs);
+});
+
+// READ ONE
+router.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate('author', 'username');
+  res.json(blog);
+});
+
+// UPDATE
+router.put('/:id', verifyToken, async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (blog.author.toString() !== req.userId) return res.status(403).send("Not your blog");
+  const updated = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+});
+
+// DELETE
+router.delete('/:id', verifyToken, async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (blog.author.toString() !== req.userId) return res.status(403).send("Not your blog");
+  await Blog.findByIdAndDelete(req.params.id);
+  res.send("Deleted");
+});
+
+module.exports = router;
